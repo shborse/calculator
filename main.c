b3dbba7873f4fae67937c26d9d45723ea32da7d8
student2 <student2@gmail.com>
2026-04-06 15:34:56 +0530

    Added multiplication feature

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int add(int a, int b) {
-    return a + b;
+    	return a + b;
+}
+int multiply(int a, int b) {
+    	return a * b;
+}
+int main() {
+   	 printf("Simple Calculator\n");
+   	 printf("Add: %d\n", add(5, 3));
+	printf("Multiply: %d\n", multiply(5, 3));
+   	 return 0;
 }
 
-int main() {
-    printf("Simple Calculator\n");
-    printf("Add: %d\n", add(5, 3));
-    return 0;
-}
