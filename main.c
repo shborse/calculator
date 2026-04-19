bc7c9f1384fd8e0894ed94c92e42e3c9727d4c5f
Student Two <student2@example.com>
2026-04-19 21:44:22 +0530

    Added division feature

--- main.c
+++ main.c
@@ -6,10 +6,14 @@ int add(int a, int b) {
 int multiply(int a, int b) {
     	return a * b;
 }
+int divide(int a, int b) {
+    return a / b;
+}
 int main() {
    	 printf("Final  Calculator\n");
    	 printf("Add: %d\n", add(5, 3));
 	printf("Multiply: %d\n", multiply(5, 3));
+	printf("Divide: %d\n", divide(5, 3));
    	 return 0;
 }
 
